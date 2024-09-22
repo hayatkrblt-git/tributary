b7691522b90b6a26077623f7dddea8bf1c49aa6f
hayatkrblt-git <forzoomonly00@gmail.com>
2024-09-22 20:21:08 +0200

    update entrypoint.py

--- entrypoint.py
+++ entrypoint.py
@@ -1,4 +1,4 @@
-print("hello again!")
+
 """Dependencies Defined
 
 gunicorn==21.2.0:
@@ -16,3 +16,30 @@ requests==2.31.0:
 redis==5.0.1:
 "redis" is a Python client library for interacting with Redis, which is an in-memory data store.
 "5.0.1" is the specific version of the "redis" library that your project requires."""
+import json
+import redis as redis
+from Flask import Flask, request
+from loguru import logger
+
+HISTORY_LENGTH = 10
+DATA_KEY = "engine_temperature"
+
+@app.route('/record', methods=['POST'])
+def record_engine_temperature():
+    payload = request.get_json(force=True)
+    logger.info(f"(*) record request --- {json.dumps(payload)} (*)")
+
+    engine_temperature = payload.get("engine_temperature")
+    logger.info(f"engine temperature to record is: {engine_temperature}")
+
+    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+    database.lpush(DATA_KEY, engine_temperature)
+    logger.info(f"stashed engine temperature in redis: {engine_temperature}")
+
+    while database.llen(DATA_KEY) > HISTORY_LENGTH:
+        database.rpop(DATA_KEY)
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    logger.info(f"engine temperature list now contains these values: {engine_temperature_values}")
+
+    logger.info(f"record request successful")
+    return {"success": True}, 200
---
+++ test.py
@@ -0,0 +1,4 @@
+from Flask import requests
+
+response = requests.post("http://0.0.0.0:8000/record", json={"engine_temperature": 0.3})
+print(response.json())
